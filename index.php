3485782f80542cf526da1f68d96ccd49c31ec16d
Allen Kinlock <DEFCAST@DEFCAST-MBP.local>
2018-03-05 12:46:08 -0800

    Initial savvy commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+<head>
+
+    <title> Savvy Watch</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/resources/css/fonts.css">
+    <link rel="stylesheet" href="/resources/css/theme.css">
+    <link rel="stylesheet" href="/resources/css/cnc.css">
+    <link rel="stylesheet" href="/resources/css/cnc-icons.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/resources/css/flexslider.css">
+    <link rel="stylesheet" href="/resources/css/animate.min.css">
+    <link rel="stylesheet" href="/resources/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Dosis|Maven+Pro|Quicksand|Rajdhani" rel="stylesheet"> 
+
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="/resources/js/modernizr-custom.js"></script>
+    <script src="/resources/js/jquery.flexslider-min.js"></script>
+
+    
+    <script src="/resources/js/modernizr-custom.js"></script>
+    <script src="/resources/js/jquery.flexslider-min.js"></script>
+    <script src="/resources/js/cnc.js"></script>
+</head>
+<body>
+<object type="image/svg+xml" data="cnc-logo.svg">
+  <img src="cnc-live.png" alt="cnc technologies" />
+</object><object type="image/svg+xml" data="cnc-ani.svg">
+  <img src="fallback-image.png" alt="…" />
+</object>
+</body>
+</html>
