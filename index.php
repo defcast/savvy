ddeab8024969b0a1df000c064a17c794017fcd75
Allen Wheeler <awheeler@defcast.io>
2018-03-07 16:16:13 -0800

    update index.php

--- index.php
+++ index.php
@@ -8,33 +8,62 @@
 <!--[if !(IE 7) & !(IE 8)]><!-->
 <html lang="en-US">
 <!--<![endif]-->
-<head>
+  <head>
 
-    <title> Savvy Watch</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/resources/css/fonts.css">
-    <link rel="stylesheet" href="/resources/css/theme.css">
-    <link rel="stylesheet" href="/resources/css/cnc.css">
-    <link rel="stylesheet" href="/resources/css/cnc-icons.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/resources/css/flexslider.css">
-    <link rel="stylesheet" href="/resources/css/animate.min.css">
-    <link rel="stylesheet" href="/resources/css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Dosis|Maven+Pro|Quicksand|Rajdhani" rel="stylesheet"> 
+      <title> Savvy Watch</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="/resources/css/fonts.css">
+      <link rel="stylesheet" href="/resources/css/theme.css">
+      <link rel="stylesheet" href="/resources/css/cnc.css">
+      <link rel="stylesheet" href="/resources/css/cnc-icons.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="/resources/css/flexslider.css">
+      <link rel="stylesheet" href="/resources/css/animate.min.css">
+      <link rel="stylesheet" href="/resources/css/style.css">
+      <link href="https://fonts.googleapis.com/css?family=Comfortaa:300|Dosis|Maven+Pro|Quicksand|Rajdhani" rel="stylesheet"> 
 
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="/resources/js/modernizr-custom.js"></script>
-    <script src="/resources/js/jquery.flexslider-min.js"></script>
+      <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+      <script src="/resources/js/modernizr-custom.js"></script>
+      <script src="/resources/js/jquery.flexslider-min.js"></script>
 
-    
-    <script src="/resources/js/modernizr-custom.js"></script>
-    <script src="/resources/js/jquery.flexslider-min.js"></script>
-    <script src="/resources/js/cnc.js"></script>
-</head>
-<body>
-<object type="image/svg+xml" data="resources/img/cnc-live-svgo.svg">
-  <img src="resources/img/cnc-live.png" alt="cnc technologies" />
-</object>
-</body>
+      
+      <script src="/resources/js/modernizr-custom.js"></script>
+      <script src="/resources/js/jquery.flexslider-min.js"></script>
+      <script src="/resources/js/cnc.js"></script>
+  </head>
+  <body>
+    <a href="index.php">
+      <object type="image/svg+xml" data="resources/img/cnc-live-svgo.svg">
+        <img src="resources/img/cnc-live.png" alt="cnc technologies" />
+      </object>
+    </a>
+    <ul>
+      <li>
+        <a href="live/">Live</a>
+      </li>
+      <li>
+        <a href="map/">Map</a>
+      </li>
+      <li>
+        <a href="videos/">Videos</a>
+      </li>
+      <li>
+        <a href="photos/">Photos</a>
+      </li>
+      <li>
+        <a href="help/">Help</a>
+      </li>
+      <li>
+        <a href="upgrade/">Upgrade</a>
+      </li>
+    </ul>
+    <hr>
+    <?php
+      phpinfo();
+    ?>
+    <div>
+      <h1>CNC Live Video Management System</h1>
+    </div>
+  </body>
 </html>
